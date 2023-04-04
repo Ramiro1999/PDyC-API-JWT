2a7e533a8382b65a69377529780b96fc5eb80e2a
Ramiro1999 <rami.garcia.orsi@gmail.com>
2023-04-04 09:35:02 -0300

    first commit

---
+++ src/test/java/unnoba/edu/tp2/Tp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package unnoba.edu.tp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
